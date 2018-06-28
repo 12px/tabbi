6f59d7c8f06359468906c45237473e94b56fd2e6
12px <dalton@12px.io>
2018-06-28 14:59:28 -0500

    Start PinBoard layout.

---
+++ src/components/PinBoard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="home">
+
     <tool-bar></tool-bar>
+    <pin-board></pin-board>
+
   </div>
 </template>
 
 <script>
   import ToolBar from '../components/ToolBar.vue'
+  import PinBoard from '../components/PinBoard.vue'
 
   export default {
-    components: { ToolBar }
+    components: { ToolBar, PinBoard }
   }
 </script>
 
