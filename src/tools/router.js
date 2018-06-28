525c9a34489ba41c0749f07f37f6ba0082aea8df
12px <dalton@12px.io>
2018-06-28 09:02:21 -0500

    Add Auth and Router scripts.

---
+++ src/tools/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home.vue'
+import Callback from '@/views/Callback.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/callback',
+      name: 'callback',
+      component: Callback
+    }
+  ]
+})
+
+// very basic "setup" of a global guard
+router.beforeEach((to, from, next) => {
+  if(to.name == 'callback') { // check if "to"-route is "callback" and allow access
+    next()
+  } else if (router.app.$auth.isAuthenticated()) { // if authenticated allow access
+    next()
+  } else { // trigger auth0's login.
+    router.app.$auth.login()
+  }
+})
+
+export default router
