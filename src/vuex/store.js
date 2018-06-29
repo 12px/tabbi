3660b88b813307119aeb47a9dafa79fc3be1257b
12px <dalton@12px.io>
2018-06-29 09:23:01 -0500

    More testing data.

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -8,19 +8,53 @@ Vue.use(Vuex)
 const state = {
   // local
   ver: 1,
-  idKey: 2,
+  idKey: 3,
   lastUsed: 1,
 
   // bookmarks
   boards: [{
     id: 1,
     name: "Inbox",
-    idKey: 3,
+    idKey: 7,
     links: [{
-      id: 1, name: "pinnd - testing",
+      id: 1, name: "pinnd - testing 1",
       link: "http://localhost:4000", tags: ["dev", "home"]
     },{
       id: 2, name: "pinnd - testing 2",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 3, name: "pinnd - testing 3",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 4, name: "pinnd - testing 4",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 5, name: "pinnd - testing 5",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 6, name: "pinnd - testing 6",
+      link: "http://localhost:4000", tags: []
+    },{
+    id: 2,
+    name: "Inbox 2",
+    idKey: 7,
+    links: [{
+      id: 1, name: "pinnd - testing 1",
+      link: "http://localhost:4000", tags: ["dev", "home"]
+    },{
+      id: 2, name: "pinnd - testing 2",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 3, name: "pinnd - testing 3",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 4, name: "pinnd - testing 4",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 5, name: "pinnd - testing 5",
+      link: "http://localhost:4000", tags: []
+    },{
+      id: 6, name: "pinnd - testing 6",
       link: "http://localhost:4000", tags: []
     }]
   }],
