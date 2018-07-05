6c7262d5a9b2766b0eaac17763529f27fee297b6
12px <dalton@12px.io>
2018-07-05 14:54:43 -0500

    Change board key from / to :

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -4,7 +4,7 @@ const actions = {
     let filter = { active: false, by: 'string', key: '' }
     if (data.string) {
       let by = data.string.charAt(0)
-      filter.by = by == '/' ? 'board' : by == '#' ? 'tag' : 'string'
+      filter.by = by == ':' ? 'board' : by == '#' ? 'tag' : 'string'
       filter.key = filter.by == 'string' ? data.string : data.string.substr(1)
       filter.active = true
     }
