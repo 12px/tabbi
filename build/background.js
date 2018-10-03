5750b66eac83b6b02fd900501b8e20add4eb6241
12px <dalton@12px.io>
2018-10-03 11:53:24 -0500

    Formatting.

--- build/background.js
+++ build/background.js
@@ -2,11 +2,12 @@ chrome.runtime.onInstalled.addListener(function() {
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { urlContains: '//'},
-      })
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { urlContains: '//'},
+        })
       ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
+      actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 
