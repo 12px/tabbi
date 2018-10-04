d2de39b64bdc37ef2e9259af5a1eb8d72a204efd
12px <dalton@12px.io>
2018-10-04 13:27:19 -0500

    Overhaul InfoPane.

--- src/sections/InfoPane.vue
+++ src/sections/InfoPane.vue
@@ -2,90 +2,108 @@
   <div id="info">
     <div class="card">
       <div class="logo row">
-        <div class="col col-lg-2 col-xlg-1">
+        <div class="col">
           <h3>Tabbi</h3>
-        </div>
-        <div class="col col-lg-10 col-xlg-11">
           <h6>your (new) new tab page</h6>
+          <h5 class="float-right">
+            <a href="#" @click="close()"><i class="fas fa-times"></i></a>
+          </h5>
         </div>
       </div>
       <div class="row">
-        <div class="col col-lg-4">
-          <h6>Bookmark Focused</h6>
-          <p><strong>Tabbi</strong> puts all the links you save front and center, so you never have to dig for them. The filter bar it makes it find whichever link you're looking for, and quickly.</p>
-          <p>Using an extension, you can even save whole sessions of links!</p>
-        </div>
-        <div class="col col-lg-4">
-          <h6>Use Anywhere</h6>
-          <p>Tabbi is available as a static website and a browser extension. Since you can sync your data with google drive, you can use it anywhere.</p>
-          <p>Use a browser extension on your desktop, and set it as your home page on mobile.</p>
-        </div>
-        <div class="col col-lg-4">
-          <h6>Features</h6>
-          <p>
-            Filtering by Title, Tag or Board. <br>
-            <strong>:key</strong> filters boards - 
-            <strong>#key</strong> filters tags <br>
-            <em>Hit 'Enter' to open all visible links you've filtered</em>
-          </p>
-          <p><strong>Session Saving</strong> will save all of your open tabs into a new session, for re-opening later. (Extension Required)</p>
-        </div>
-      </div>
-      <div class="row bottom">
         <div class="col col-lg-4">
           <h6>Already Have Bookmarks?</h6>
-          <label>Import them here to get started!</label>
-          <p class="align-center">
-            <input id="import" type="file" @change="getImport">
-            <label for="import" class="full button button-primary">
-              Import Bookmarks (*.html)
-            </label>
-          </p>
+          <p>You can export them to an HTML file, and then import them here.</p>
+          <input type="file" id="import" @change="getImport">
+          <label for="import" class="align-center full button button-primary">
+            Import Bookmarks (*.html)
+          </label>
         </div>
         <div class="col col-lg-4">
-          <h6>Options</h6>
-          <div class="form-control">
-            <label>Collapse After</label>
-            <select :value="view.links" @change="setLinks">
-              <option value="3">3 Links</option>
-              <option value="5">5 Links</option>
-              <option value="10">10 Links</option>
-              <option value="25">25 Links</option>
-            </select>
-          </div>
+          <h6>Keep Data In Sync</h6>
+          <p>Tabbi can sync with your Google Drive account so you don't lose data.</p>
+          <button class="full button-primary" @click="setSync()">
+            Drive Sync: {{ synced ? 'On' : 'Off' }}
+          </button>
         </div>
         <div class="col col-lg-4">
-          <h6>Backup</h6>
-          <div class="form-control">
-            <label>Sync With Google Drive</label>
-            <button class="full button-primary" @click="setSync()">
-              Drive Sync: {{ synced ? 'On' : 'Off' }}
-            </button>
-          </div>
+          <h6>Tabbi Options</h6>
+          <p><strong>Visible Links</strong> how many links shown per card before collapsing</p>
+          <select :value="view.links" @change="setLinks">
+            <option value="3">3 Links</option>
+            <option value="5">5 Links</option>
+            <option value="10">10 Links</option>
+            <option value="25">25 Links</option>
+          </select>
         </div>
       </div>
       <div class="row">
-        <div class="col align-center">
+        <div class="col align-right">
+          <small class="float-left">
+            Use Reddit or Github to request features and report bugs.
+          </small>
+          <a @click="about = true"
+            href="#" class="button button-primary-outlined button-small">
+            About
+          </a>
           <a href="https://github.com/12px/tabbi" 
             target="_blank" class="button button-primary-outlined button-small">
             On Github
           </a>
-          <a href="https://chrome.google.com/webstore/detail/ilonjmcjjkcnhndjfkaoekocdfhedlfd"
+          <a href="https://reddit.com/r/tabbi" 
             target="_blank" class="button button-primary-outlined button-small">
+            On Reddit
+          </a>
+          <a href="https://chrome.google.com/webstore/detail/ilonjmcjjkcnhndjfkaoekocdfhedlfd"
+            target="_blank" class="button button-primary button-small">
             For Chrome
-          </a> <br>
-          <button class="button-primary button-small" @click="close()">
-            Dismiss
-          </button> <br>
-          <small>v{{ $root.version }}</small>
+          </a>
         </div>
       </div>
     </div>
+    <div class="modal-mask" v-if="about">
+      <div class="modal">
+        <div class="modal-head">
+          <strong class="modal-title">About Tabbi</strong>
+        </div>
+        <div class="modal-body">
+          <div class="accordian">
+            <h5 @click="accord = 1"><i class="fas fa-plus"></i> Bookmark Focused</h5>
+            <div v-if="accord == 1">
+              <p><strong>Tabbi</strong> puts all the links you save front and center, so you never have to dig for them. The filter bar it makes it find whichever link you're looking for, and quickly.</p>
+              <p>Using an extension, you can even save whole sessions of links!</p>
+            </div>
+
+            <h5 @click="accord = 2"><i class="fas fa-plus"></i> Use Anywhere</h5>
+            <div v-if="accord == 2">
+              <p>Tabbi is available as a static website and a browser extension. Since you can sync your data with google drive, you can use it anywhere.</p>
+              <p>Use a browser extension on your desktop, and set it as your home page on mobile.</p>
+            </div>
+
+            <h5 @click="accord = 3"><i class="fas fa-plus"></i> Features</h5>
+            <div v-if="accord == 3">
+              <p>
+                Filtering by Title, Tag or Board. <br>
+                <strong>:key</strong> filters boards - 
+                <strong>#key</strong> filters tags <br>
+                <em>Hit 'Enter' to open all visible links you've filtered</em>
+              </p>
+              <p><strong>Session Saving</strong> will save all of your open tabs into a new session, for re-opening later. (Extension Required)</p>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer align-right">
+          <small>v{{ $root.version }}</small>
+          <button class="button-primary" @click="about = false">Close</button>
+        </div>
+      </div>
+  </div>
   </div>
 </template>
 
 <script>
   export default {
+    data() { return { accord: 1, about: false } },
     computed: {
       view()   { return this.$store.state.view },
       synced() { return this.$store.state.meta.syncData },
@@ -108,11 +126,31 @@
   #info {
     padding-bottom: 15px;
   }
-  .logo {
-    align-items: center;
+  .logo .col * {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .logo h3 {
+    margin-right: 0.5em;
+  }
+  .logo h6 {
+    margin-top: 1px;;
+  }
+  .accordian h5 {
+    cursor: pointer;
+    padding-bottom: 5px;
+  }
+  .accordian .fa-plus {
+    font-size: 0.5em;
+    margin-top: -5px;
+    display: inline-block;
+    vertical-align: middle;
   }
   .card {
     margin: 0;
+  }
+  select, .button.full {
+    margin-bottom: 0;
   }
   .row .col-lg-4 {
     padding-right: 20px;
